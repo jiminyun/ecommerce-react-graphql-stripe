fa4e2adeba8dbfb238f8794bff985e03620e6b8a
Jimin <jiminyun7@gmail.com>
2019-06-01 01:46:23 -0400

    Add Signin + Signup + User authentication with jwt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,72 @@
 import React from "react";
-import { Box, Text, Heading, Image } from "gestalt";
-import { NavLink } from "react-router-dom";
+import { Box, Text, Heading, Image, Button } from "gestalt";
+import { getToken, clearToken, clearCart } from "../utils";
+import { NavLink, withRouter } from "react-router-dom";
 
-const Navbar = () => (
+class Navbar extends React.Component {
+  handleSignout = () => {
+    clearToken();
+    clearCart();
+    this.props.history.push("/");
+  };
+
+  render() {
+    return getToken() !== null ? (
+      <AuthNav handleSignout={this.handleSignout} />
+    ) : (
+      <UnAuthNav />
+    );
+  }
+}
+
+const AuthNav = ({ handleSignout }) => (
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="around"
+    height={70}
+    color="midnight"
+    padding={1}
+    shape="roundedBottom"
+  >
+    {/* Checkout Link */}
+    <NavLink activeClassName="active" to="/checkout">
+      <Text size="xl" color="white">
+        Checkout
+      </Text>
+    </NavLink>
+
+    {/* Title and Logo */}
+    <NavLink activeClassName="active" exact to="/">
+      <Box display="flex" alignItems="center">
+        <Box margin={2} height={50} width={50}>
+          <Image
+            alt="BrewHaha Logo"
+            naturalHeight={1}
+            naturalWidth={1}
+            src="./icons/logo.svg"
+          />
+        </Box>
+        <div className="main-title">
+          <Heading size="xs" color="orange">
+            BrewHaha
+          </Heading>
+        </div>
+      </Box>
+    </NavLink>
+
+    {/* Signout Button */}
+    <Button
+      onClick={handleSignout}
+      color="transparent"
+      text="Sign Out"
+      inline
+      size="md"
+    />
+  </Box>
+);
+
+const UnAuthNav = () => (
   <Box
     display="flex"
     alignItems="center"
@@ -30,9 +94,12 @@ const Navbar = () => (
             src="./icons/logo.svg"
           />
         </Box>
-        <Heading size="xs" color="orange">
-          BrewHaha
-        </Heading>
+        {/* Title */}
+        <div className="main-title">
+          <Heading size="xs" color="orange">
+            BrewHaha
+          </Heading>
+        </div>
       </Box>
     </NavLink>
 
@@ -45,4 +112,4 @@ const Navbar = () => (
   </Box>
 );
 
-export default Navbar;
+export default withRouter(Navbar);
